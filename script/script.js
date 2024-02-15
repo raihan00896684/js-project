38aa252fbe4b7264e1c33ea1afe80394503fedf6
raihan00896684 <155303509+raihan00896684@users.noreply.github.com>
2024-02-16 00:23:59 +0600

    first commit

---
+++ script/script.js
@@ -0,0 +1,43 @@
+function start(){
+
+    getShow ()
+  geThidden()
+
+  key()
+
+}
+
+function getShow (){
+const show = document.getElementById('animal');
+show.classList.remove('hidden')
+console.log(show);
+getcat 
+}
+
+
+function   geThidden(){
+  const element = document.getElementById('home');
+  element.classList.add('hidden')
+  console.log(element)
+}
+
+   
+
+
+function key(){
+   const tig = document.getElementById('tiger');
+
+}
+function getcat (){
+   const show = document.getElementById('cat');
+  show.classList.remove('hidden')
+  console.log(show);
+   }
+
+if( input = 'a'){
+
+const show = document.getElementById('tiger');
+  show.classList.add('hidden')
+  console.log(show);
+
+}
